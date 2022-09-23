090ef0463d3856c34c075b8832c569bfe1b40ba6
patel-divyang <hello@divyangpatel.com>
2022-09-22 18:16:42 -0600

    Initial Commit

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -87,43 +87,38 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=11b5dd75e69a4984b481323122
         else if (
             code == 1003 ||
             code == 1006 ||
-            code == 1009 ||
-            code == 1030 ||
-            code == 1069 ||
-            code == 1087 ||
-            code == 1282 ||
             code == 1135 ||
-            code == 1273 ||
-            code == 1276 ||
-            code == 1279 
+            code == 1147 ||
+            code == 1150
         ) {
             bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/cloudy.jpg)`;
             bg.style.backgroundSize = "cover";
         }
         else if (
-            code == 1063 ||
+            code == 1066 ||
             code == 1069 ||
             code == 1072 ||
-            code == 1150 ||
-            code == 1153 ||
-            code == 1180 ||
-            code == 1183 ||
-            code == 1186 ||
-            code == 1189 ||
-            code == 1192 ||
-            code == 1195 ||
+            code == 1114 ||
+            code == 1117 ||
+            code == 1210 ||
+            code == 1213 ||
+            code == 1216 ||
+            code == 1219 ||
+            code == 1222 ||
+            code == 1225 ||
+            code == 1237 ||
+            code == 1252 ||
+            code == 1258 ||
+            code == 1261 ||
+            code == 1264 ||
             code == 1204 ||
             code == 1207 ||
-            code == 1240 ||
-            code == 1243 ||
-            code == 1246 ||
-            code == 1249 ||
-            code == 1252 
+            code == 1249 
         ) {
-            bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/rainy.jpg)`;
+            bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/snowy.jpg)`;
             bg.style.backgroundSize = "cover";
         } else {
-            bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/snowy.jpg)`;
+            bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/rainy.jpg)`;
             bg.style.backgroundSize = "cover";
         }
 
@@ -167,9 +162,7 @@ function fetchWeatherData() {
         let year = logDate.substring(0,4);
         let month = logDate.substring(5,7);
         let day = logDate.substring(8,10);
-
-        
-
+      
         
         if (month == 01 ) {
             month = "January";
@@ -219,41 +212,36 @@ function fetchWeatherData() {
         else if (
             code == 1003 ||
             code == 1006 ||
-            code == 1009 ||
-            code == 1030 ||
-            code == 1069 ||
-            code == 1087 ||
-            code == 1282 ||
             code == 1135 ||
-            code == 1273 ||
-            code == 1276 ||
-            code == 1279 
+            code == 1147 ||
+            code == 1150 
         ) {
             bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/cloudy.jpg)`;
         }
         else if (
-            code == 1063 ||
+            code == 1066 ||
             code == 1069 ||
             code == 1072 ||
-            code == 1150 ||
-            code == 1153 ||
-            code == 1180 ||
-            code == 1183 ||
-            code == 1186 ||
-            code == 1189 ||
-            code == 1192 ||
-            code == 1195 ||
+            code == 1114 ||
+            code == 1117 ||
+            code == 1210 ||
+            code == 1213 ||
+            code == 1216 ||
+            code == 1219 ||
+            code == 1222 ||
+            code == 1225 ||
+            code == 1237 ||
+            code == 1252 ||
+            code == 1258 ||
+            code == 1261 ||
+            code == 1264 ||
             code == 1204 ||
             code == 1207 ||
-            code == 1240 ||
-            code == 1243 ||
-            code == 1246 ||
-            code == 1249 ||
-            code == 1252 
+            code == 1249
         ) {
-            bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/rainy.jpg)`;
-        } else {
             bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/snowy.jpg)`;
+        } else {
+            bg.style.backgroundImage = `url(./assets/img/${timeOfDay}/rainy.jpg)`;
         }
 
         bg.style.opacity = "1";
